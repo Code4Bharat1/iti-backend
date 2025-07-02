75980b96d00e4350a21d59182e9d4e08a9d3d5cb
mariyask04 <skmariya484@gmail.com>
2025-07-02 15:28:45 +0530

    delete

--- models/Image.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const imageSchema = new mongoose.Schema({
-  imageUrl: { type: String, required: true },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "Admin" },
-}, { timestamps: true });
-
-export const Image = mongoose.model("Image", imageSchema);
--- models/Video.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const videoSchema = new mongoose.Schema({
-  videoUrl: { type: String, required: true },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "Admin" },
-}, { timestamps: true });
-
-export const Video = mongoose.model("Video", videoSchema);
